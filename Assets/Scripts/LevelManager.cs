ce67764aa4702ce1a84ac4822a964e58905cb1fd
Satasuk Vipaksinlapin <satasuk@gmail.com>
2017-10-17 16:11:13 +0700

    Start the project and make main menu

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void loadLevel(string name){
+		Debug.Log ("Clicked Level load requested for : "+name);
+		Application.LoadLevel (name);
+	}
+	public void quitRequest(){
+		Debug.Log ("Requested for quit");
+		Application.Quit();
+	}
+	public void loadNextLevel(){
+		Application.LoadLevel (Application.loadedLevel + 1); //loaded level return int
+		//TODO add a maximum load level when complete
+	}
+
+}
