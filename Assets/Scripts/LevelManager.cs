2e84dfe04f7f4c6d4979fbfa949dab2cae48be3b
Satasuk Vipaksinlapin <satasuk@gmail.com>
2017-10-17 20:02:12 +0700

    Create a Space Ship and make it moveable

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class LevelManager : MonoBehaviour {
 
 	public void loadLevel(string name){
 		Debug.Log ("Clicked Level load requested for : "+name);
-		Application.LoadLevel (name);
+		//Application.LoadLevel (name);
+		SceneManager.LoadScene(name);
 	}
 	public void quitRequest(){
 		Debug.Log ("Requested for quit");
 		Application.Quit();
 	}
 	public void loadNextLevel(){
-		Application.LoadLevel (Application.loadedLevel + 1); //loaded level return int
-		//TODO add a maximum load level when complete
+		//Application.LoadLevel (Application.loadedLevel + 1); //loaded level return int
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
 	}
 
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour {
+	public float playerSpeed=8.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.LeftArrow))
+			MoveLeft();
+		if (Input.GetKey (KeyCode.RightArrow))
+			MoveRight ();
+	}
+	void MoveLeft(){
+		this.transform.position += new Vector3 (-playerSpeed*Time.deltaTime,0,0); //Time.deltaTime this multiply factor prevent when the script loads slower or faster the framerate
+		//PS. Make sure movement is independent of framerate using Time.deltaTime
+		//now the player will move smoothly
+	}
+	void MoveRight(){
+		//this.transform.position = new Vector2 (this.transform.position.x+playerSpeed*Time.deltaTime, this.transform.position.y); Error if use + operator because this position is in vector3
+		this.transform.position += new Vector3 (playerSpeed*Time.deltaTime,0,0);
+	}
+}
