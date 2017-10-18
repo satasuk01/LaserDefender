2efe0865551bd07b538adb4527f3790c8f4fe9f5
Satasuk vipaksinlapin <satasuk@gmail.com>
2017-10-18 08:58:31 +0700

    Make enemy prefab and enemy spawner

---
+++ Assets/Entities/Enemies/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemyPrefab;
+	// Use this for initialization
+	void Start () {
+		GameObject enemy = Instantiate (enemyPrefab,new Vector3(0,0,0),new Quaternion(0,0,-180,0))/*as GameObject */; //rotate 180
+		enemy.transform.parent= transform; //make enemy attached to game Obj.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
